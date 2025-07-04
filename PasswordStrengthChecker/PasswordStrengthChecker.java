4d2765ae7f02aa507c5054101a788a68e21968c6
KamaleshTGK <151112692+KamaleshTGK@users.noreply.github.com>
2025-07-04 21:30:24 +0530

    Create PasswordStrengthChecker.java

---
+++ PasswordStrengthChecker/PasswordStrengthChecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PasswordStrengthChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine();
+
+        boolean hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch))
+                hasUpper = true;
+            else if (Character.isLowerCase(ch))
+                hasLower = true;
+            else if (Character.isDigit(ch))
+                hasDigit = true;
+            else
+                hasSpecial = true;
+        }
+
+        if (password.length() >= 8 && hasUpper && hasLower && hasDigit && hasSpecial) {
+            System.out.println("Password strength: Strong");
+        } else if (password.length() >= 6 &&((hasUpper && hasLower)) || (hasLower && hasDigit) || (hasUpper && hasDigit)) {
+            System.out.println("Password strength: Medium");
+        } else {
+            System.out.println("Password strength: Weak");
+        }
+    }
+}
